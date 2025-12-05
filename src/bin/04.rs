34a91428735ac14373d2c21cbb8748c7835f9896
Carlos A. Parra F. <carlos.a.parra.f@proton.me>
2025-12-05 02:47:15 +0100

    feat: finished day 4 part 1

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -1,7 +1,76 @@
 advent_of_code::solution!(4);
 
+fn parse_input(input: &str) -> Vec<Vec<usize>> {
+    input
+        .split('\n')
+        .map(|line| {
+            line.chars()
+                .map(|char| match char {
+                    '.' => 0,
+                    _ => 1,
+                })
+                .collect()
+        })
+        .collect()
+}
+
 pub fn part_one(input: &str) -> Option<u64> {
-    None
+    let rolls_map = parse_input(input);
+    let total_rows = rolls_map.len();
+    let total_columns = rolls_map[0].len();
+    let mut total_accessible = 0;
+    for y in 0..total_rows {
+        for x in 0..total_columns {
+            if rolls_map[y][x] == 0 {
+                print!("·");
+                continue;
+            }
+            let top = if y > 0 { rolls_map[y - 1][x] } else { 0 };
+            let bot = if y < total_rows - 1 {
+                rolls_map[y + 1][x]
+            } else {
+                0
+            };
+            let left = if x > 0 { rolls_map[y][x - 1] } else { 0 };
+            let right = if x < total_columns - 1 {
+                rolls_map[y][x + 1]
+            } else {
+                0
+            };
+            let top_left = if x > 0 && y > 0 {
+                rolls_map[y - 1][x - 1]
+            } else {
+                0
+            };
+            let top_right = if x < total_columns - 1 && y > 0 {
+                rolls_map[y - 1][x + 1]
+            } else {
+                0
+            };
+
+            let bot_left = if x > 0 && y < total_rows - 1 {
+                rolls_map[y + 1][x - 1]
+            } else {
+                0
+            };
+
+            let bot_right = if x < total_columns - 1 && y < total_rows - 1 {
+                rolls_map[y + 1][x + 1]
+            } else {
+                0
+            };
+            let total_blocking =
+                top + bot + left + right + top_left + top_right + bot_left + bot_right;
+            if total_blocking < 4 {
+                print!("{total_blocking}");
+                total_accessible += 1;
+                continue;
+            }
+            print!("@");
+        }
+        println!();
+    }
+    Some(total_accessible)
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
@@ -15,7 +84,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(13));
     }
 
     #[test]
